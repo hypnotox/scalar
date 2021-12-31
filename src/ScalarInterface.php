f3b1d114cab8175dc0183923b9459a0dd81f191b
hypnotox <hypnotox@users.noreply.github.com>
2021-12-31 00:37:03 +0000

    chore: run php-cs-fixer

--- src/ScalarInterface.php
+++ src/ScalarInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace HypnoTox\Scalars;
 
 interface ScalarInterface extends \Stringable
--- src/StringObject.php
+++ src/StringObject.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace HypnoTox\Scalars;
 
 class StringObject implements ScalarInterface
